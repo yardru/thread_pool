6cc8151989178f383f67015b6375236fc47d9a61
yardru <yardru@yandex.ru>
2017-07-13 19:25:15 +0300

    first working version

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -1,1 +1,82 @@
 #include "thread_pool.h"
+
+
+ds::THREAD_POOL::THREAD_POOL(COUNT threadsNumber)
+{
+   for (COUNT i = 0; i < threadsNumber; i++) {
+      THREAD * pThread = new THREAD(*this);
+      threads.push_back(std::unique_ptr<THREAD>(pThread));
+      freeThreads.push(pThread);
+   }
+}
+
+
+std::future<int> ds::THREAD_POOL::AddTask(const TASK & task)
+{
+   PACKAGED_TASK packagedTask(task);
+   auto future = packagedTask.get_future();
+
+   std::unique_lock<std::mutex> lock(mutex);
+   if (!freeThreads.empty()) {
+      freeThreads.front()->SetTask(std::move(packagedTask));
+      freeThreads.pop();
+   } else {
+      packagedTasks.push(std::move(packagedTask));
+   }
+
+   return future;
+}
+
+
+void ds::THREAD_POOL::AddFreeThread(THREAD & thread)
+{
+   std::unique_lock<std::mutex> lock(mutex);
+   if (!packagedTasks.empty()) {
+      thread.SetTask(std::move(packagedTasks.front()));
+      packagedTasks.pop();
+   }
+   else {
+      freeThreads.push(&thread);
+   }
+}
+
+
+ds::THREAD_POOL::THREAD::THREAD(THREAD_POOL & pool) : pool(pool),
+   stdThread(ThreadFunc, std::ref(*this)), isClose(false)
+{
+}
+
+
+ds::THREAD_POOL::THREAD::~THREAD(void)
+{
+   isClose = true;
+   isTaskUpdated.notify_all();
+   if (stdThread.joinable()) {
+      stdThread.join();
+   }
+}
+
+
+void ds::THREAD_POOL::THREAD::SetTask(PACKAGED_TASK && packagedTask)
+{
+   std::unique_lock<std::mutex> lock(mutex);
+   task = std::move(packagedTask);
+   isTaskUpdated.notify_all();
+}
+
+
+void ds::THREAD_POOL::THREAD::ThreadFunc(THREAD & thread)
+{
+   std::mutex utilMutex;
+   while (!thread.isClose) {
+      std::unique_lock<std::mutex> lock(utilMutex);
+      thread.isTaskUpdated.wait(lock, [&task = thread.task, &isClose = thread.isClose]() {
+         return task.valid() || isClose;
+      });
+      if (thread.task.valid()) {
+         thread.task();
+         thread.SetTask(PACKAGED_TASK());
+         thread.pool.AddFreeThread(thread);
+      }
+   }
+}
--- thread_pool.h
+++ thread_pool.h
@@ -1,8 +1,12 @@
 #ifndef _DS_THREAD_POOL_H_
 #define _DS_THREAD_POOL_H_
 
+#include <list>
+#include <queue>
+#include <thread>
 #include <future>
 #include <functional>
+#include <mutex>
 
 namespace ds {
 
@@ -10,9 +14,33 @@ class THREAD_POOL {
 public:
    using COUNT = unsigned int;
    using TASK = std::function<int(void)>;
+   using PACKAGED_TASK = std::packaged_task<int(void)>;
 
-   THREAD_POOL(COUNT threads_number);
+   THREAD_POOL(COUNT threadsNumber);
    std::future<int> AddTask(const TASK & task);
+private:
+   class THREAD {
+   public:
+      THREAD(THREAD_POOL & pool);
+      ~THREAD(void);
+      void SetTask(PACKAGED_TASK && packagedTask);
+   private:
+      static void ThreadFunc(THREAD & thread);
+
+      THREAD_POOL & pool;
+      PACKAGED_TASK task;
+      std::mutex mutex;
+      std::condition_variable isTaskUpdated;
+      bool isClose;
+      std::thread stdThread;
+   };
+
+   void AddFreeThread(THREAD & thread);
+
+   std::queue<PACKAGED_TASK> packagedTasks;
+   std::vector<std::unique_ptr<THREAD>> threads;
+   std::queue<THREAD *> freeThreads;
+   std::mutex mutex;
 };
 
 } // namespace ds
